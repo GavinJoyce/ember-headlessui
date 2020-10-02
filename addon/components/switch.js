f2a914773310eadfc0c2fa5cc12f559ac349eb86
Gavin Joyce <gavinjoyce@gmail.com>
2020-10-02 21:14:04 +0100

    initial `<Switch>` component

---
+++ addon/components/switch.js
@@ -0,0 +1,35 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { guidFor } from "@ember/object/internals";
+
+export default class Switch extends Component {
+  guid = `${guidFor(this)}-tailwindui-switch`;
+  labelGuid = `${this.guid}-label`;
+  buttonGuid = `${this.guid}-button`;
+
+  @action
+  onLabelPointerUp() {
+    let buttonElement = document.getElementById(this.buttonGuid);
+    buttonElement.click();
+    buttonElement.focus();
+  }
+
+  @action
+  onButtonClick(event) {
+    event.preventDefault();
+    this._toggle();
+  }
+
+  @action
+  onButtonKeyUp(event) {
+    event.preventDefault();
+    if (event.key === " ") {
+      event.preventDefault();
+      this._toggle();
+    }
+  }
+
+  _toggle() {
+    this.args.onUpdate(!this.args.isChecked);
+  }
+}
