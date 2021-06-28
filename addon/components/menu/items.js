e46741d43ef94f903e812ee67b2e5dcd9bfc66f6
Alex LaFroscia <alex@lafroscia.com>
2021-06-28 10:17:27 -0400

    fix(menu): remove `Menu` DOM node
    This change brings the Ember implementation closer inline with the React/Vue implementations, which do not create a DOM node for this component automatically. This makes working with the component easier, as it no longer introduces un-necessary DOM nodes.
    
    Note: the React and Vue implementations allow for optionally creating a DOM node for the `Menu` component. This is possible in Ember, technically, but probably more trouble than it's worth, since the user of `Menu` can just wrap the component in an additional DOM node themselves if that's what they want. I believe the React and Vue implementations include this functionality for consistency, so that _all_ Headless UI components can optionally render an alternate DOM node, but those frameworks have an easier ability to pull this off than Ember does.
    
    A focus trap has been introduced to power the click-outside-to-dismiss functionality, which also resolves the issue of focus needing to be locked in the `Menu::Items` component when rendered, which was previously not the case.
    
    BREAKING CHANGE: If you were previously depending on `Menu` rendering a DOM node, you'll want to wrap your usage of `Menu` with a `div` and move any attributes or modifiers to that element instead.

--- addon/components/menu/items.js
+++ addon/components/menu/items.js
@@ -2,6 +2,10 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class Items extends Component {
+  get menuItemsElement() {
+    return document.getElementById(this.args.itemsGuid);
+  }
+
   @action
   onKeydown(event) {
     switch (event.key) {
@@ -21,5 +25,22 @@ export default class Items extends Component {
           return this.args.search(event.key);
         }
     }
+  }
+
+  clickInsideMenuTrigger(event) {
+    const buttonElement = document.getElementById(this.args.buttonGuid);
+
+    // The `buttonElement` could not exist if the element has been removed from the DOM
+    return buttonElement ? buttonElement.contains(event.target) : false;
+  }
+
+  @action
+  allowClickOutsideFocusTrap(event) {
+    return this.clickInsideMenuTrigger(event);
+  }
+
+  @action
+  clickOutsideFocusTrapDeactivates(event) {
+    return !this.clickInsideMenuTrigger(event);
   }
 }
