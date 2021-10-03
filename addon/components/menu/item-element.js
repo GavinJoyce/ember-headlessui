48855999cd98039d4f52eca290d9f3d7aa061ee2
Gavin Joyce <gavinjoyce@gmail.com>
2021-10-03 15:24:41 +0100

    create a `tag-name-is-component` helper

--- addon/components/menu/item-element.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class MenuItemElement extends Component {
-  get tagNameIsComponent() {
-    return typeof this.args.tagName === 'object';
-  }
-}
---
+++ app/helpers/tag-name-is-component.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-headlessui/helpers/tag-name-is-component';
