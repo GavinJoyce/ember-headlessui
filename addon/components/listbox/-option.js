4e0e260132fdd95d17ddca9ba8edaf0e7e34b85b
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-12-06 21:25:44 -0500

    Remove render modifiers from listbox/options

--- addon/components/listbox/-option.js
+++ addon/components/listbox/-option.js
@@ -3,8 +3,16 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 
+import { modifier } from 'ember-modifier';
+
 export default class ListboxOptionComponent extends Component {
   @tracked guid = `${guidFor(this)}-headlessui-listbox-option`;
+
+  registration = modifier((element) => {
+    this.args.registerOptionsElement(element);
+
+    return () => this.args.unregisterOptionsElement(element);
+  });
 
   @action
   handleClick(e) {
