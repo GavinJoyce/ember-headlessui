dca0e23b33c9f8124b4360abaf07638a6dd68c53
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-05-29 18:22:50 -0400

    Add custom colors back

--- tests/dummy/config/tailwind.config.js
+++ tests/dummy/config/tailwind.config.js
@@ -3,9 +3,23 @@
 // const defaultTheme = require('tailwindcss/defaultTheme');
 
 let extensions = 'html,hbs,js,ts,gjs,gts';
-let paths = ['./tests/dummy/**/*', './addon/**/*']
+let paths = ['./tests/dummy/**/*', './addon/**/*'];
 
 module.exports = {
-  content: paths.map(glob => `${glob}.{${extensions}}`),
+  content: paths.map((glob) => `${glob}.{${extensions}}`),
+  theme: {
+    extend: {
+      colors: {
+        code: {
+          green: '#b5f4a5',
+          yellow: '#ffe484',
+          purple: '#d9a9ff',
+          red: '#ff8383',
+          blue: '#93ddfd',
+          white: '#fff',
+        },
+      },
+    },
+  },
   plugins: [require('@tailwindcss/typography')],
 };
