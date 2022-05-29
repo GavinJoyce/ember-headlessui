8dcc4c24273ccaa9f1e30d7e8c3d85069eed2847
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-05-29 12:07:38 -0400

    Try re-naming in case there are shadowing issues

--- tests/integration/components/dialog-test.js
+++ tests/integration/components/dialog-test.js
@@ -602,28 +602,30 @@ module('Integration | Component | <Dialog>', function (hooks) {
   });
 
   module('Nesting', function () {
-    for (let { strategy, action, test } of [
+    for (let { strategy, action, doTest } of [
       {
-        test: todo,
+        doTest: todo,
         strategy: 'outside click',
         action: () => click(document.body),
       },
       {
-        test,
+        doTest: test,
         strategy: 'ESCAPE click',
         action: () =>
           triggerKeyEvent(document.activeElement, 'keyup', Keys.Escape),
       },
       {
-        test,
+        doTest: test,
         strategy: 'click on Dialog.Overlay',
         action: () => click(getDialogOverlays().pop()),
       },
     ]) {
-      test(`it should be possible to open nested Dialog components and close them with '${strategy}'`, async function (assert) {
-        this.isOpen = false;
+      doTest(
+        `it should be possible to open nested Dialog components and close them with '${strategy}'`,
+        async function (assert) {
+          this.isOpen = false;
 
-        await render(hbs`
+          await render(hbs`
           <button
             type="button"
             {{on 'click' (set this "isOpen" true)}}>
@@ -636,249 +638,250 @@ module('Integration | Component | <Dialog>', function (hooks) {
           />
         `);
 
-        assert.strictEqual(
-          getDialogs().length,
-          0,
-          'Verify we have no open dialogs'
-        );
+          assert.strictEqual(
+            getDialogs().length,
+            0,
+            'Verify we have no open dialogs'
+          );
 
-        await click(getByText('Open 1'), 'Open Dialog 1');
+          await click(getByText('Open 1'), 'Open Dialog 1');
 
-        assert.strictEqual(
-          getDialogs().length,
-          1,
-          'Verify that we have 1 open dialog'
-        );
+          assert.strictEqual(
+            getDialogs().length,
+            1,
+            'Verify that we have 1 open dialog'
+          );
 
-        await assertActiveElement(
-          getByText('Open 2 a'),
-          'Verify that the `Open 2 a` has focus'
-        );
+          await assertActiveElement(
+            getByText('Open 2 a'),
+            'Verify that the `Open 2 a` has focus'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 2 b'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 2 b'),
+            'Verify that we can tab around'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 2 c'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 2 c'),
+            'Verify that we can tab around'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 2 a'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 2 a'),
+            'Verify that we can tab around'
+          );
 
-        // Open Dialog 2 via the second button
-        await click(getByText('Open 2 b'));
+          // Open Dialog 2 via the second button
+          await click(getByText('Open 2 b'));
 
-        assert.strictEqual(
-          getDialogs().length,
-          2,
-          'Verify that we have 2 open dialogs'
-        );
+          assert.strictEqual(
+            getDialogs().length,
+            2,
+            'Verify that we have 2 open dialogs'
+          );
 
-        await assertActiveElement(
-          getByText('Open 3 a'),
-          'Verify that the `Open 3 a` has focus'
-        );
+          await assertActiveElement(
+            getByText('Open 3 a'),
+            'Verify that the `Open 3 a` has focus'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 3 b'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 3 b'),
+            'Verify that we can tab around'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 3 c'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 3 c'),
+            'Verify that we can tab around'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 3 a'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 3 a'),
+            'Verify that we can tab around'
+          );
 
-        //close the top most dialog with ${strategy}
-        await action();
+          //close the top most dialog with ${strategy}
+          await action();
 
-        assert.strictEqual(
-          getDialogs().length,
-          1,
-          'Verify that we have 1 open dialog'
-        );
+          assert.strictEqual(
+            getDialogs().length,
+            1,
+            'Verify that we have 1 open dialog'
+          );
 
-        await assertActiveElement(
-          getByText('Open 2 b'),
-          'Verify that the `Open 2 b` button got focused again'
-        );
+          await assertActiveElement(
+            getByText('Open 2 b'),
+            'Verify that the `Open 2 b` button got focused again'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 2 c'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 2 c'),
+            'Verify that we can tab around'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 2 a'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 2 a'),
+            'Verify that we can tab around'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 2 b'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 2 b'),
+            'Verify that we can tab around'
+          );
 
-        // Open Dialog 2 via the second button
-        await click(getByText('Open 2 b'));
+          // Open Dialog 2 via the second button
+          await click(getByText('Open 2 b'));
 
-        await assertActiveElement(
-          getByText('Open 3 a'),
-          'Verify that the `Open 3 a` has focus'
-        );
+          await assertActiveElement(
+            getByText('Open 3 a'),
+            'Verify that the `Open 3 a` has focus'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 3 b'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 3 b'),
+            'Verify that we can tab around'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 3 c'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 3 c'),
+            'Verify that we can tab around'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 3 a'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 3 a'),
+            'Verify that we can tab around'
+          );
 
-        assert.strictEqual(
-          getDialogs().length,
-          2,
-          'Verify that we have 2 open dialogs'
-        );
+          assert.strictEqual(
+            getDialogs().length,
+            2,
+            'Verify that we have 2 open dialogs'
+          );
 
-        // Open Dialog 3 via button c
-        await click(getByText('Open 3 c'));
+          // Open Dialog 3 via button c
+          await click(getByText('Open 3 c'));
 
-        await assertActiveElement(
-          getByText('Open 4 a'),
-          'Verify that the `Open 4 a` has focus'
-        );
+          await assertActiveElement(
+            getByText('Open 4 a'),
+            'Verify that the `Open 4 a` has focus'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 4 b'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 4 b'),
+            'Verify that we can tab around'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 4 c'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 4 c'),
+            'Verify that we can tab around'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 4 a'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 4 a'),
+            'Verify that we can tab around'
+          );
 
-        assert.strictEqual(
-          getDialogs().length,
-          3,
-          'Verify that we have 3 open dialogs'
-        );
+          assert.strictEqual(
+            getDialogs().length,
+            3,
+            'Verify that we have 3 open dialogs'
+          );
 
-        //close the top most dialog with ${strategy}
-        await action();
+          //close the top most dialog with ${strategy}
+          await action();
 
-        await assertActiveElement(
-          getByText('Open 3 c'),
-          'Verify that the `Open 3 c` button got focused again'
-        );
+          await assertActiveElement(
+            getByText('Open 3 c'),
+            'Verify that the `Open 3 c` button got focused again'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 3 a'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 3 a'),
+            'Verify that we can tab around'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 3 b'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 3 b'),
+            'Verify that we can tab around'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 3 c'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 3 c'),
+            'Verify that we can tab around'
+          );
 
-        assert.strictEqual(
-          getDialogs().length,
-          2,
-          'Verify that we have 2 open dialogs'
-        );
+          assert.strictEqual(
+            getDialogs().length,
+            2,
+            'Verify that we have 2 open dialogs'
+          );
 
-        //close the top most dialog with ${strategy}
-        await action();
+          //close the top most dialog with ${strategy}
+          await action();
 
-        assert.strictEqual(
-          getDialogs().length,
-          1,
-          'Verify that we have 1 open dialog'
-        );
+          assert.strictEqual(
+            getDialogs().length,
+            1,
+            'Verify that we have 1 open dialog'
+          );
 
-        await assertActiveElement(
-          getByText('Open 2 b'),
-          'Verify that the `Open 2 b` button got focused again'
-        );
+          await assertActiveElement(
+            getByText('Open 2 b'),
+            'Verify that the `Open 2 b` button got focused again'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 2 c'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 2 c'),
+            'Verify that we can tab around'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 2 a'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 2 a'),
+            'Verify that we can tab around'
+          );
 
-        userEvent.tab();
-        await assertActiveElement(
-          getByText('Open 2 b'),
-          'Verify that we can tab around'
-        );
+          userEvent.tab();
+          await assertActiveElement(
+            getByText('Open 2 b'),
+            'Verify that we can tab around'
+          );
 
-        //close the top most dialog with ${strategy}
-        await action();
+          //close the top most dialog with ${strategy}
+          await action();
 
-        assert.strictEqual(
-          getDialogs().length,
-          0,
-          'Verify that we have 0 open dialogs'
-        );
+          assert.strictEqual(
+            getDialogs().length,
+            0,
+            'Verify that we have 0 open dialogs'
+          );
 
-        await assertActiveElement(
-          getByText('Open 1'),
-          'Verify that the `Open 1` got focused again'
-        );
-      });
+          await assertActiveElement(
+            getByText('Open 1'),
+            'Verify that the `Open 1` got focused again'
+          );
+        }
+      );
     }
   });
 
