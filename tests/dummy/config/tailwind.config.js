1d8217a14ed7d79f987785e1c1c596827ebbd1dc
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-05-29 18:20:45 -0400

    Upgrade to Tailwind 3, use prose plugin from typography

--- tests/dummy/config/tailwind.config.js
+++ tests/dummy/config/tailwind.config.js
@@ -1,28 +1,11 @@
 // NOTE: we should try an stay in sync with https://github.com/tailwindlabs/headlessui/blob/develop/tailwind.config.js
 
-const defaultTheme = require('tailwindcss/defaultTheme');
+// const defaultTheme = require('tailwindcss/defaultTheme');
+
+let extensions = 'html,hbs,js,ts,gjs,gts';
+let paths = ['./tests/dummy/**/*', './addon/**/*']
 
 module.exports = {
-  purge: [],
-  theme: {
-    container: {
-      center: true,
-    },
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        code: {
-          green: '#b5f4a5',
-          yellow: '#ffe484',
-          purple: '#d9a9ff',
-          red: '#ff8383',
-          blue: '#93ddfd',
-          white: '#fff',
-        },
-      },
-    },
-  },
-  variants: {},
+  content: paths.map(glob => `${glob}.{${extensions}}`),
+  plugins: [require('@tailwindcss/typography')],
 };
