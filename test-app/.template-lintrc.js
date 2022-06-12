03c2d4d4a674d8e0f5fbd1b8ed107d80b0930de6
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-06-12 13:28:07 -0400

    Sync template lint configs

--- test-app/.template-lintrc.js
+++ test-app/.template-lintrc.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended',
+  plugins: ['ember-template-lint-plugin-prettier'],
+
+  extends: ['octane', 'ember-template-lint-plugin-prettier:recommended'],
+
+  rules: {
+    'no-down-event-binding': 'warn',
+  },
 };
