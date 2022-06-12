e5f10d9d5d4da7fc295209ebfdd8f7d717d399f0
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-06-12 19:07:27 -0400

    Reapply blueprint to addon and fix peers, etc

--- ember-headlessui/tests/dummy/config/environment.js
+++ ember-headlessui/tests/dummy/config/environment.js
@@ -5,7 +5,7 @@ module.exports = function (environment) {
     modulePrefix: 'dummy',
     environment,
     rootURL: '/',
-    locationType: 'auto',
+    locationType: 'history',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -45,7 +45,6 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.rootURL = '/ember-headlessui';
   }
 
   return ENV;
